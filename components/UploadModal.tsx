906b37269415bc4b97c71cfdc8b072e01c0f8d5a
Ingo Schröder <inschro@gmail.com>
2023-09-29 19:47:16 +0200

    fixed Capital CamelCase

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -11,7 +11,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
 
 
-const uploadModal = () => {
+const UploadModal = () => {
     const [isLoading, setIsLoading] = useState(false);
     const uploadModal = useUploadModal();
     const { user } = useUser();
@@ -173,4 +173,4 @@ const uploadModal = () => {
     )
 }
 
-export default uploadModal
+export default UploadModal
